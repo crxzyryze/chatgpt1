7af96d64f2b0e7189347022d7cb4aa119d45d5aa
crxzyryze <kent.tran.3107.2011@outlook.com>
2025-06-27 14:20:43 +1000

    Create server.js

---
+++ server.js
@@ -0,0 +1,1 @@
+const express = require('express'); const path = require('path'); const app = express(); // serve everything in this folder app.use(express.static(path.join(__dirname))); const PORT = process.env.PORT || 3000; app.listen(PORT, () => console.log(`Listening on ${PORT}`));
